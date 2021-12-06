23fe5d05596ca692a186987b438283c758b5aeb1
honehone <honehone.41games@gmail.com>
2021-12-06 10:26:43 +0900

    first commit

---
+++ serial_servo_for_2links.ino
@@ -0,0 +1,141 @@
+#include "twin_feedback_rotation_servo.h"
+
+#define SERIAL_SERVO_BYTE_TYPE 0x00
+#define SERIAL_SERVO_FLOAT_TYPE 0x01
+#define SERIAL_SERVO_BAUD_RATE 115200
+#define SERIAL_SERVO_TOTAL_COMMUNICATE_BYTES 14
+#define SERIAL_SERVO_MAX_FALSE_COUNT 255
+#define SERIAL_SERVO_TIME_OUT 500 //0.5sec
+
+static const byte header[]
+{
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+};
+static const byte writing_data_description[] 
+{
+    0x08, 0x04, 0x02, SERIAL_SERVO_FLOAT_TYPE
+};
+
+ServoSet servo_set;
+WritingData writing_data;
+Commands commands;
+unsigned long time_last_cmd;
+
+extern HardwareSerial Serial;
+
+void readCommands()
+{
+    byte header_count(0);
+    byte false_count(0);
+    while(header_count < 8)
+    {
+        if(Serial.read() == 0xff)
+        {
+            header_count++;
+        }
+        else
+        {
+            header_count = 0;
+            false_count++;
+            if(false_count >= SERIAL_SERVO_MAX_FALSE_COUNT)
+            {
+                //return nothing ??
+                return;
+            }
+        }
+    }
+
+    if(
+        Serial.read() == 0x02 && Serial.read() == 0x01 &&
+        Serial.read() == 0x02 && Serial.read() == SERIAL_SERVO_BYTE_TYPE 
+    )
+    {
+        byte cmd_upper(Serial.read());
+        byte cmd_lower(Serial.read());
+        if(
+            cmd_upper >= ROTATION_SERVO_CCW_MAX && 
+            cmd_upper <= ROTATION_SERVO_CW_MAX
+        )
+        {
+            commands.command_upper = cmd_upper;
+        }
+        if(
+            cmd_lower >= ROTATION_SERVO_CCW_MAX &&
+            cmd_lower <= ROTATION_SERVO_CW_MAX
+        )
+        {
+            commands.command_lower = cmd_lower;    
+        }
+    }
+}
+
+void setup()
+{
+    servo_set.upper_servo_ptr = new Servo();
+    servo_set.lower_servo_ptr = new Servo();
+    servoInit(&servo_set);
+
+    commands.command_upper = ROTATION_SERVO_STOPPING;
+    commands.command_lower = ROTATION_SERVO_STOPPING;
+    time_last_cmd = 0;
+
+    Serial.begin(SERIAL_SERVO_BAUD_RATE);
+
+    while (!Serial)
+    {
+        delay(10);
+    }
+}
+
+void loop()
+{
+    if(Serial)
+    {
+        Serial.flush();
+
+        unsigned long time_now(millis());
+        // just in case of overflow
+        if(time_now < time_last_cmd)
+        {
+            time_last_cmd = 0;
+        }
+  
+        if(Serial.available() >= SERIAL_SERVO_TOTAL_COMMUNICATE_BYTES)
+        {
+            readCommands();
+            servo_set.upper_servo_ptr->write(commands.command_upper);
+            servo_set.lower_servo_ptr->write(commands.command_lower);
+            time_last_cmd = time_now;
+
+            //60fps loop
+            delay(15);
+        }
+
+        if(time_now - time_last_cmd > SERIAL_SERVO_TIME_OUT)
+        {
+            prepareExit(&servo_set);
+        }
+
+        readFeedBack(&writing_data);
+        
+        Serial.write(
+            header,
+            8
+        );
+        Serial.write(
+            writing_data_description,
+            4
+        );
+        // for test
+        //writing_data.feedbacks.upper_feedback = 1.2345f;
+        //writing_data.feedbacks.lower_feedback = 5.6789f;
+        Serial.write(
+            writing_data.bin,
+            8
+        );
+    }
+    else
+    {
+        prepareExit(&servo_set);
+    }
+}
---
+++ twin_feedback_rotation_servo.h
@@ -0,0 +1,91 @@
+#ifndef FEEDBACK_ROTATION_SERVO_H_
+#define FEEDBACK_ROTATION_SERVO_H_
+
+#include "Arduino.h"
+#include "Servo.h"
+
+#define SERVO_SET_UPPER_PIN 10
+#define SERVO_SET_LOWER_PIN 9
+
+#define SERVO_PULSE_MAX_UPPER 2000
+#define SERVO_PULSE_MIN_UPPER 1000
+#define SERVO_PULSE_MAX_LOWER 2000
+#define SERVO_PULSE_MIN_LOWER 1000
+
+#define ROTATION_SERVO_STOPPING 90
+#define ROTATION_SERVO_CW_MAX 180
+#define ROTATION_SERVO_CCW_MAX 0
+
+#define RAW_2_VOL 0.0047483381f //5.0v / 1053
+#define VOL_2_ANG 109.0909090909f //360deg / 3.3v
+#define ONE_ROUND_DEG 360.0f
+
+struct ServoSet
+{
+    Servo* upper_servo_ptr;
+    Servo* lower_servo_ptr;
+};
+
+struct Feedbacks
+{
+    float upper_feedback;
+    float lower_feedback;
+};
+
+struct Commands
+{
+    uint8_t command_upper;
+    uint8_t command_lower;
+};
+
+union WritingData
+{
+    struct Feedbacks feedbacks;
+    uint8_t bin[8];
+};
+
+void prepareExit(const ServoSet* servo_set_ptr)
+{
+    servo_set_ptr->upper_servo_ptr->write(ROTATION_SERVO_STOPPING);
+    servo_set_ptr->lower_servo_ptr->write(ROTATION_SERVO_STOPPING);
+}
+
+void servoInit(const ServoSet* servo_set_ptr)
+{
+    servo_set_ptr->upper_servo_ptr->attach(
+        SERVO_SET_UPPER_PIN,
+        SERVO_PULSE_MAX_UPPER, 
+        SERVO_PULSE_MIN_UPPER
+    );
+    servo_set_ptr->lower_servo_ptr->attach(
+        SERVO_SET_LOWER_PIN,
+        SERVO_PULSE_MAX_LOWER,
+        SERVO_PULSE_MIN_LOWER
+    );
+    servo_set_ptr->upper_servo_ptr->write(ROTATION_SERVO_STOPPING);
+    servo_set_ptr->lower_servo_ptr->write(ROTATION_SERVO_STOPPING);
+}
+
+void readFeedBack(WritingData* writing_data)
+{
+    int raw_upper(analogRead(PIN_A1));
+    int raw_lower(analogRead(PIN_A0));
+    float volt_upper(raw_upper * RAW_2_VOL); 
+    float volt_lower(raw_lower * RAW_2_VOL);
+    float angle_upper(volt_upper * VOL_2_ANG);
+    float angle_lower(volt_lower * VOL_2_ANG);
+    
+    if(angle_upper > ONE_ROUND_DEG)
+    {
+        angle_upper -= ONE_ROUND_DEG;
+    }
+    if(angle_lower > ONE_ROUND_DEG)
+    {
+        angle_lower -= ONE_ROUND_DEG;
+    }
+
+    writing_data->feedbacks.upper_feedback = angle_upper;
+    writing_data->feedbacks.lower_feedback = angle_lower;
+}
+
+#endif
